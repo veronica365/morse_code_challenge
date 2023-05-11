8d725aff5270f7089926fc8115bd17a0bbebc45a
veronica365 <naruth10@gmail.com>
2023-05-11 06:15:18 +0300

    add functionality for the morse code

---
+++ morse_code.rb
@@ -0,0 +1,16 @@
+def decode(string)
+    morse_code_table = {
+        '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+        '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+        '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+        '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+        '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+        '--..' => 'Z', "*@*" => " "
+    }
+    new_string = string
+    decoded_message = ""
+    new_string["   "] = " *@* "
+    string_split = new_string.split(" ")
+    string_split.each{ |code| decoded_message += morse_code_table[code]}
+    return decoded_message
+end
