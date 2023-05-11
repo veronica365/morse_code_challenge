0de0023d6bbe22ece733dabace2054ba95367a63
veronica365 <naruth10@gmail.com>
2023-05-11 06:23:50 +0300

    Modify name for the linters folder

--- morse_code.rb
+++ morse_code.rb
@@ -1,16 +1,15 @@
 def decode(string)
-    morse_code_table = {
-        '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-        '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-        '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-        '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-        '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-        '--..' => 'Z', "*@*" => " "
-    }
-    new_string = string
-    decoded_message = ""
-    new_string["   "] = " *@* "
-    string_split = new_string.split(" ")
-    string_split.each{ |code| decoded_message += morse_code_table[code]}
-    return decoded_message
+  morse_code_table = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F',
+    '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+    '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z', '*@*' => ' '
+  }
+  new_string = string
+  decoded_message = ''
+  new_string.gsub! '   ', ' *@* '
+  string_split = new_string.split
+  string_split.each { |code| decoded_message += morse_code_table[code] }
+  puts decoded_message
 end
