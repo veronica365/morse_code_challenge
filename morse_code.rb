5d1af4acb571310e96a9f50b55fa7fa3e3d6c1f3
veronica365 <naruth10@gmail.com>
2023-05-12 17:02:43 +0300

    Add functionality for the other three methods

--- morse_code.rb
+++ morse_code.rb
@@ -1,4 +1,4 @@
-def decode(string)
+def decode_char(char)
   morse_code_table = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F',
     '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
@@ -6,10 +6,26 @@ def decode(string)
     '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
     '-.--' => 'Y', '--..' => 'Z', '*@*' => ' '
   }
+  morse_code_table[char]
+end
+
+def decode_word(word)
+  decoded_message = ''
+  word.split.each { |code| decoded_message += decode_char(code) }
+  decoded_message
+end
+
+def decode_sentence(string)
   new_string = string
   decoded_message = ''
   new_string.gsub! '   ', ' *@* '
   string_split = new_string.split
-  string_split.each { |code| decoded_message += morse_code_table[code] }
-  puts decoded_message
+  string_split.each { |code| decoded_message += decode_char(code) }
+  decoded_message
 end
+
+puts decode_char('.-')
+
+puts decode_word('-- -.--')
+
+puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
